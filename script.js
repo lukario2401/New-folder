76e64a4da0ef7afc3af8bd56c971d92e473085e5
lukario2401 <lukadolidze433@gmail.com>
2023-06-03 22:53:15 +0400

    beauty added

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let img = document.getElementById('img');
 let kmg = document.getElementById('kmg');
 let info = document.getElementById('info');
+let but = document.getElementById('but');
 let mode = 1;
+but.style.backgroundColor="green";
 function fun(){
     if(mode==1){
         img.src="infected.png";
         kmg.src="asthma.png";
         info.style.display="block";
         mode=0;
+        but.innerHTML= `<h2>view normal </h2>`;
+        but.style.backgroundColor="red";
     }else{
         img.src="bio.png"
         kmg.src="";
         info.style.display="none";
         mode=1;
+        but.innerHTML= `<h2>view asthma</h2>`;
+        but.style.backgroundColor="green";
     }
     
 }
